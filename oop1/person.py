e79a50879ccef9be42e507a75967a2bbdb90bf41
Sync9733 <644608020011@dpu.ac.th>
2022-06-14 13:03:41 +0700

    person

--- oop1/person.py
+++ oop1/person.py
@@ -1,9 +1,10 @@
 class Person:
+    country = 'Thailand' #Class variable
     def __init__(self,name,gender,proffession,study) -> None:
-        self.name = name
-        self.gender = gender
-        self.profession = proffession
-        self.study2 = study
+        self.name = name #instance variable
+        self.gender = gender #instance variable
+        self.profession = proffession #instance variable
+        self.study2 = study #instance variable
 
     def work(self):
         print(f'{self.name} is working as a {self.profession}')
@@ -19,10 +20,27 @@ person_obj.work()
 person_obj.study()
 person_obj.show()
 
-person2_obj = Person('John','Male','Professional',15)
-person2_obj.work()
-person2_obj.study()
-person2_obj.show()
+John = Person('John','Male','Professional',15)
+John.work()
+John.study()
+John.show()
 
-person3_obj = Person('Lisa','Female','Korean Singer',15)
-person3_obj.work()
+Lisa = Person('Lisa','Female','Korean Singer',15)
+Lisa.work()
+
+print(f'Class Variable : {Person.country}')
+print(f'Instance Variable : {Lisa.country}')
+
+#assign value
+Lisa.country = 'Korea'
+print('-----------')
+print(f'Class Variable : {Person.country}')
+print(f'Instance Variable : {Lisa.country}')
+print(f'Instance Variable : {John.country}')
+
+#assign Class variable
+Person.country = 'England'
+print('-----------')
+print(f'Class Variable : {Person.country}')
+print(f'Instance Variable : {Lisa.country}')
+print(f'Instance Variable : {John.country}')
