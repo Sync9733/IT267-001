80de52baa8958069aed94f1a92c916b4c14fb366
Sync9733 <644608020011@dpu.ac.th>
2022-06-20 11:17:46 +0700

    CoffeeOrder

--- oop1/coffee_order.py
+++ oop1/coffee_order.py
@@ -1,1 +1,34 @@
-class CoffeeOrder
+class CoffeeOrder:
+    menu_type = 'Coffee'
+    total = 0
+    def __init__(self,customer_name:str,menu:str,price:float,size:str = 'R',num:int = 1) -> None:
+        self.customer = customer_name
+        self.menu = menu
+        self.num = num
+        self.size = size
+        self.price = price
+
+    def check_menu(self):
+        pass
+
+    def compute_price(self):
+        if self.size == 'L':
+            self.price = self.price +1
+        elif self.size == 'XL':
+            self.price = self.price +1.5
+        else:
+            self.price
+        CoffeeOrder.total = self.num * self.price
+
+    def display_detail(self):
+        print('---- Coffee Order ----')
+        print(f'Name  : {self.customer}')
+        print(f'Menu  : {self.menu}')
+        print(f'Size  : {self.size}')
+        print(f'Num   : {self.num}')
+        print(f'Price : {self.price}')
+        print(f'Total : {self.total}')
+
+if __name__ == '__main__' :
+    John = CoffeeOrder('John','Espresso')
+    John.display_detail()
