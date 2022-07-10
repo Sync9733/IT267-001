7eabb2fbf777db313d8fe1fb8351008f83453463
Sync9733 <644608020011@dpu.ac.th>
2022-07-10 09:38:04 +0700

    juicemachine

---
+++ examination/exam01/juicemachine.py
@@ -0,0 +1,33 @@
+class Juiceorder:
+    def __init__(self,menu:str,size:str) -> None:
+        self.menu = menu.upper()
+        self.size = size.upper()
+        self.price = 0
+    
+    def check_menu(self):
+        if self.menu == 'ORANGE JUICE':
+           self.menu = 'OJ'
+           self.price = 25
+
+        elif self.menu == 'APPLE JUICE':
+            self.menu = 'AJ'
+            self.price = 25
+
+        elif self.menu == 'WATERMELON JUICE':
+            self.menu = 'WJ'
+            self.price = 30
+
+        elif self.menu == 'PINEAPPLE JUICE':
+            self.menu = 'PJ'
+            self.price = 30
+        
+    def compute_price(self):
+        if self.size == 'L':
+            self.price = self.price + 5
+        else:
+            self.price
+    
+    def display_order(self):
+        self.check_menu()
+        self.compute_price()
+        print(f'{self.menu}({self.size}*{self.price})=>{self.price} baht')
---
+++ examination/exam01/juicemachineobj.py
@@ -0,0 +1,10 @@
+from juicemachine import Juiceorder
+
+wj = Juiceorder('Watermelon Juice','L')
+wj.display_order()
+
+oj = Juiceorder('Orange Juice','R')
+oj.display_order()
+
+pj = Juiceorder('Pineapple Juice','L')
+pj.display_order()
